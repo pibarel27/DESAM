13d6c09591e2033a0ae625f35e31cf1091eb7610
Pibarel Maisnam <maisnamakashsingh@gmail.com>
2024-05-03 22:04:25 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import Preloader from "./layout/Preloader";
 const Home = lazy(() => import("./ui/Home"));
 const Career = lazy(() => import("./ui/Career"));
 const Contact = lazy(() => import("./ui/Contact"));
 const About = lazy(() => import("./ui/About"));
 const Service = lazy(() => import("./ui/Service"));
-const NoPage = lazy(() => import("./layout/NoPage"));
 
 function App() {
   return (
     <>
       <Router>
-        <Suspense fallback={ <Preloader/>}>
+        <Suspense fallback={ <Home/>}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="services" element={<Service />} />
             <Route path="careers" element={<Career />} />
             <Route path="contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} />       
+            <Route path="*" element={<Home />} />       
           </Routes>
         </Suspense>
       </Router> 
